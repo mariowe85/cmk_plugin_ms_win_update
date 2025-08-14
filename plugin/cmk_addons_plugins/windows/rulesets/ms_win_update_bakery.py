50df386ce75113ec71d63166af42a66eb504f965
Mario <43858936+mariowe85@users.noreply.github.com>
2025-08-14 15:48:04 +0200

    Update ms_win_update_bakery.py
    rewrite with sccm support

--- plugin/cmk_addons_plugins/windows/rulesets/ms_win_update_bakery.py
+++ plugin/cmk_addons_plugins/windows/rulesets/ms_win_update_bakery.py
@@ -2,6 +2,7 @@
 # -*- coding: utf-8; py-indent-offset: 4; max-line-length: 100 -*-
 
 # Copyright (C) 2025  Christopher Pommer <cp.software@outlook.de>
+# Enhanced with SCCM support by Mario Fellner <mario.fellner@outlook.at>
 
 # This program is free software; you can redistribute it and/or
 # modify it under the terms of the GNU General Public License
@@ -18,19 +19,21 @@
 # Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 
 ####################################################################################################
-# CHECKMK RULESET: Microsoft Windows Update (bakery plug-in)
+# CHECKMK RULESET: Microsoft Windows Update with SCCM Support (bakery plug-in)
 #
-# This file defines the deployment parameters for the ms_win_update.ps1 agent plug-in.
+# This file defines the deployment parameters for the ms_win_update_v2.ps1 agent plug-in.
 ####################################################################################################
 
 from cmk.rulesets.v1 import Title, Help
 from cmk.rulesets.v1.form_specs import (
+    BooleanChoice,
     CascadingSingleChoice,
     CascadingSingleChoiceElement,
     DefaultValue,
     DictElement,
     Dictionary,
     FixedValue,
+    InputHint,
     TimeMagnitude,
     TimeSpan,
 )
@@ -38,19 +41,20 @@ from cmk.rulesets.v1.form_specs import (
 from cmk.rulesets.v1.rule_specs import AgentConfig, Topic
 
 
-def _valuespec_agent_config_ms_win_update():
+def _valuespec_agent_config_ms_win_update_v2():
     return Dictionary(
         title=Title("Agent Plugin Parameters"),
         help_text=Help(
-            "This will deploy an agent plugin to check the Windows updates on a Windows host."
+            "This will deploy an enhanced agent plugin to check Windows updates from both "
+            "Windows Update and SCCM sources on a Windows host."
         ),
         elements={
             "deployment": DictElement(
                 parameter_form=CascadingSingleChoice(
                     title=Title("Deployment"),
                     help_text=Help(
-                        "Select <tt>Deploy the plugin</tt> to install the agent plugin "
-                        "<tt>ms_win_update.ps1</tt> on the host."
+                        "Select <tt>Deploy the plugin</tt> to install the enhanced agent plugin "
+                        "<tt>ms_win_update_v2.ps1</tt> on the host."
                     ),
                     elements=[
                         CascadingSingleChoiceElement(
@@ -68,7 +72,9 @@ def _valuespec_agent_config_ms_win_update():
                                             title=Title("Run Asynchronously"),
                                             help_text=Help(
                                                 "Define an interval to limit the execution of "
-                                                "the plugin on the host to save resources."
+                                                "the plugin on the host to save resources. "
+                                                "Recommended: 300 seconds (5 minutes) or higher "
+                                                "as update checks can be resource intensive."
                                             ),
                                             displayed_magnitudes=[
                                                 TimeMagnitude.MINUTE,
@@ -76,6 +82,39 @@ def _valuespec_agent_config_ms_win_update():
                                                 TimeMagnitude.DAY,
                                             ],
                                             prefill=DefaultValue(300.0),
+                                        ),
+                                    ),
+                                    "enable_sccm": DictElement(
+                                        parameter_form=BooleanChoice(
+                                            title=Title("Enable SCCM Monitoring"),
+                                            help_text=Help(
+                                                "Enable monitoring of SCCM (System Center Configuration Manager) "
+                                                "updates in addition to Windows Update. This will check for "
+                                                "pending updates managed by SCCM and monitor the SCCM client status."
+                                            ),
+                                            prefill=InputHint(True),
+                                        ),
+                                    ),
+                                    "enable_windows_update": DictElement(
+                                        parameter_form=BooleanChoice(
+                                            title=Title("Enable Windows Update Monitoring"),
+                                            help_text=Help(
+                                                "Enable monitoring of standard Windows Update. "
+                                                "You may want to disable this if your environment "
+                                                "exclusively uses SCCM for update management."
+                                            ),
+                                            prefill=InputHint(True),
+                                        ),
+                                    ),
+                                    "debug_mode": DictElement(
+                                        parameter_form=BooleanChoice(
+                                            title=Title("Enable Debug Mode"),
+                                            help_text=Help(
+                                                "Enable debug output in the agent plugin for troubleshooting. "
+                                                "Debug information will be written to the Windows Event Log. "
+                                                "Disable this in production environments."
+                                            ),
+                                            prefill=InputHint(False),
                                         ),
                                     ),
                                 },
@@ -90,9 +129,9 @@ def _valuespec_agent_config_ms_win_update():
     )
 
 
-rule_spec_agent_config_ms_win_update = AgentConfig(
-    title=Title("Microsoft Windows Update"),
+rule_spec_agent_config_ms_win_update_v2 = AgentConfig(
+    title=Title("Microsoft Windows Update with SCCM"),
     topic=Topic.OPERATING_SYSTEM,
-    name="ms_win_update",
-    parameter_form=_valuespec_agent_config_ms_win_update,
+    name="ms_win_update_v2",
+    parameter_form=_valuespec_agent_config_ms_win_update_v2,
 )
